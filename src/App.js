cba17539c6b44040ce48434ac72a5f1f12500c02
unknown <srinukkuruva@gmail.com>
2024-05-24 17:23:35 +0530

    map usage in react

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
-import Button from "./components/button/button.js";
-import List from './components/list/list.js';
-import Image from './components/images/image.js';
-import Table from './components/table/table.js';
-import Form from './components/forms/form .js';
-import Ternary from './components/ternary/ternary.js';
-import Heading1,{Heading2,Heading3,Heading4,Heading5} from './components/headings/heading.js'
 
-function Hello(){
-  return(
-<div>
-  <h1>hello world</h1>
-    <Button/>
-    <List/>
-    <Image/>
-    <Table/>
-    <Form/>
-    <Ternary/>
-    <Heading1/>
-    <Heading2/>
-    <Heading3/>
-    <Heading4/>
-    <Heading5/>
-</div>
-  )
+
+
+
+const Hello = ()=>{
+const employees=[
+  {
+    name:"srinu",
+    role:"full stack developer",
+    salary:100000
+  },
+  {
+    name:"uday",
+    role:"data base developer",
+    salary:150000
+  },
+  {
+    name:"shekar",
+    role:"react  developer",
+    salary:200000
+  },
+  {
+    name:"bharath",
+    role:"back end developer",
+    salary:250000
+  }
+]
+return(
+  <>
+  {
+    employees.map((eachemployee,index)=>{
+      const {name,role,salary}=eachemployee
+      return(
+        <div>
+          <h1>{index+1}</h1>
+          <h1>{name}</h1>
+          <h2>{role}</h2>
+          <h3>{salary}</h3>
+        </div>
+      )
+    })
+  }
+  </>
+)
 }
 export default Hello
